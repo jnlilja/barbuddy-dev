425b7d3cbf59f6cec13aa2c430305e9a92ed8d43
jnlilja <jnlilja@gmail.com>
2025-02-24 14:58:28 -0800

    Initial Commit

---
+++ BarBuddy/BarBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarBuddyApp.swift
+//  BarBuddy
+//
+//  Created by Jessica Lilja on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BarBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
