8ff77c76ee0e1c361f6fe822bc2933fbe3d17a4b
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-02-28 17:58:52 -0800

    Path for the NavigationStack

--- BarBuddy/Model/NavigationDestinations.swift
+++ BarBuddy/Model/NavigationDestinations.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+// Data type representing all NavigationStack paths
+enum NavigationDestinations {
+    case nameEntry
+    case location
+    case gender
+    case hometown
+    case school
+    case drink
+    case smoking
+    case photoPrompt
+    case photoUpload
+}
