ad3975acbf6e5da70485989adc8665ca0d99f54b
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-03-06 16:48:43 -0800

    Buisness logic for displaying map content

---
+++ BarBuddy/Core/Home/ViewModel/MapViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  MapViewModel.swift
+//  BarBuddy
+//
+//  Created by Andrew Betancourt on 3/5/25.
+//
+
+import Foundation
+@preconcurrency import _MapKit_SwiftUI
+
+
+@MainActor
+@Observable
+final class MapViewModel: ObservableObject {
+    var results: [MKMapItem] = []
+    var lookAroundScene: MKLookAroundScene?
+    var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
+    
+    func searchResults(for searchText: String) async {
+        if let userLocation = await getUserLocation() {
+            
+            let request = MKLocalSearch.Request()
+            request.naturalLanguageQuery = searchText
+            request.region = .init(center: userLocation, latitudinalMeters: 2000, longitudinalMeters: 2000)
+            
+            let results = try? await MKLocalSearch(request: request).start()
+            self.results.append(results?.mapItems.first ?? MKMapItem())
+        }
+    }
+    
+    func updateCameraPosition() async {
+        guard let userLocation = await getUserLocation() else { return }
+        cameraPosition = .region(.init(center: userLocation, latitudinalMeters: 3000, longitudinalMeters: 3000))
+    }
+    
+    private func getUserLocation() async -> CLLocationCoordinate2D? {
+        let updates = CLLocationUpdate.liveUpdates()
+        
+        do {
+            let update = try await updates.first { @Sendable place in
+                place.location?.coordinate != nil
+            }
+            return update?.location?.coordinate
+            
+        }catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
