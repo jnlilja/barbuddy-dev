e00b67ebcec0018dac148a69b033b253d4549605
BrandonGarate177 <brandongarate177@gmail.com>
2025-03-19 18:57:29 -0700

    Revert "FirebaseSetup"
    This reverts commit f62b8892af4bb41748c73d61ee770b747f05762f.

--- BarBuddy/BarBuddyApp.swift
+++ BarBuddy/BarBuddyApp.swift
@@ -6,26 +6,10 @@
 //
 
 import SwiftUI
-// import FirebaseCore
-
-
-// so that firebase is connnected when the app starts 
-// class AppDelegate: NSObject, UIApplicationDelegate {
-//   func application(_ application: UIApplication,
-//                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//     FirebaseApp.configure()
-
-//     return true
-//   }
-// }
-
 
 @main
 struct BarBuddyApp: App {
     var mapViewModel = MapViewModel()
-    
-    
-    // @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
