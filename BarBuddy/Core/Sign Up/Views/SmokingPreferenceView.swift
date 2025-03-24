b994d269e61d8a82776633d05f2ceec75a3faad4
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-03-23 23:41:02 -0700

    Moved sign up directory

---
+++ BarBuddy/Core/Sign Up/Views/SmokingPreferenceView.swift
@@ -0,0 +1,107 @@
+//
+//  SmokingPreferenceView.swift
+//  BarBuddy
+//
+//  Created by Andrew Betancourt on 2/25/25.
+//
+
+
+import SwiftUI
+
+struct SmokingPreferenceView: View {
+    @State private var smokesWeed = false
+    @State private var smokesTobacco = false
+    @State private var vapes = false
+    @State private var proceedToNextPage = false
+    @Binding var path: NavigationPath
+    
+    var body: some View {
+        
+        ZStack {
+            Color("DarkBlue")
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                ProgressDots(currentPage: 6, totalPages: 7)
+                
+                VStack(spacing: 25) {
+                    Text("Do you smoke?")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .bold()
+                        .multilineTextAlignment(.center)
+                    
+                    VStack(spacing: 20) {
+                        Button(action: {
+                            smokesWeed.toggle()
+                        }) {
+                            HStack(spacing: 15) {
+                                Image(systemName: smokesWeed ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(Color("Salmon"))
+                                    .font(.system(size: 20))
+                                
+                                Text("Cannabis 🍃")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                            }
+                        }
+                        .frame(width: 200)
+                        
+                        Button(action: {
+                            smokesTobacco.toggle()
+                        }) {
+                            HStack(spacing: 15) {
+                                Image(systemName: smokesTobacco ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(Color("Salmon"))
+                                    .font(.system(size: 20))
+                                
+                                Text("Cigarettes 🚬")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                            }
+                        }
+                        .frame(width: 200)
+                        
+                        Button(action: {
+                            vapes.toggle()
+                        }) {
+                            HStack(spacing: 15) {
+                                Image(systemName: vapes ? "checkmark.square.fill" : "square")
+                                    .foregroundColor(Color("Salmon"))
+                                    .font(.system(size: 20))
+                                
+                                Text("Vape 💨")
+                                    .foregroundColor(.white)
+                                    .font(.title3)
+                            }
+                        }
+                        .frame(width: 200)
+                    }
+                    .padding(.vertical, 30)
+                    
+                    Button(action: {
+                        proceedToNextPage = true
+                        path.append(NavigationDestinations.photoPrompt)
+                    }) {
+                        Text("Continue")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 300, height: 50)
+                            .background(Color("DarkPurple"))
+                            .cornerRadius(10)
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+
+#Preview("Smoking") {
+    SmokingPreferenceView(path: .constant(NavigationPath()))
+}
