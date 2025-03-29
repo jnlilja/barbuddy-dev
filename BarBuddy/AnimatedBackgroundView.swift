9156813cd2ce10c1b90f7c335de67cf5d98b1609
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-03-29 13:52:51 -0700

    Moved to different directory

--- BarBuddy/AnimatedBackgroundView.swift
+++
@@ -1,6 +0,0 @@
-MeshGradient(width: 3, height: 3, points: [
-                        [0.0, 0.0], [0.5, 0.0], [1.0, 0.0],
-                        [0.0,0.51], [0.8, 0.2], [1.0, 0.5],
-                        [0.0, 1.01], [0.5, 1.01], [1.0, 1.0]
-                    ], colors: [.darkBlue, .darkPurple, .darkBlue, .darkPurple, .salmon, .salmon, .salmon, .darkBlue, .nude])
-                        .ignoresSafeArea()
