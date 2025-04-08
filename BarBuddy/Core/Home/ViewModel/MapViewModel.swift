5e3499e1a634ba9377f6019ef00139cbd1ee0a2b
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-04-07 20:33:32 -0700

    Formatting changes

--- BarBuddy/Core/Home/ViewModel/MapViewModel.swift
+++ BarBuddy/Core/Home/ViewModel/MapViewModel.swift
@@ -14,102 +14,170 @@ final class MapViewModel: ObservableObject {
     var lookAroundScene: MKLookAroundScene?
     var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
     private var pacificBeachCoordinate: CLLocationCoordinate2D = .init(
-        latitude: 32.794, longitude: -117.253)
-    
+        latitude: 32.794,
+        longitude: -117.253
+    )
+
     let bars: [Bar] = [
         Bar(
             name: "Mavericks Beach Club",
             location: CLLocationCoordinate2D(
-                latitude: 32.7969526, longitude: -117.2543182)),
+                latitude: 32.7969526,
+                longitude: -117.2543182
+            )
+        ),
         Bar(
             name: "Thrusters Lounge",
             location: CLLocationCoordinate2D(
-                latitude: 32.7982187, longitude: -117.2558549)),
+                latitude: 32.7982187,
+                longitude: -117.2558549
+            )
+        ),
         Bar(
             name: "710 Beach Club",
             location: CLLocationCoordinate2D(
-                latitude: 32.7964687, longitude: -117.2565146)),
+                latitude: 32.7964687,
+                longitude: -117.2565146
+            )
+        ),
         Bar(
             name: "Open Bar",
             location: CLLocationCoordinate2D(
-                latitude: 32.7937602, longitude: -117.2547777)),
+                latitude: 32.7937602,
+                longitude: -117.2547777
+            )
+        ),
         Bar(
             name: "The Grass Skirt",
             location: CLLocationCoordinate2D(
-                latitude: 32.7955066, longitude: -117.2528919)),
+                latitude: 32.7955066,
+                longitude: -117.2528919
+            )
+        ),
         Bar(
             name: "Hideaway",
             location: CLLocationCoordinate2D(
-                latitude: 32.7961859, longitude: -117.2558475)),
+                latitude: 32.7961859,
+                longitude: -117.2558475
+            )
+        ),
         Bar(
             name: "Flamingo Deck",
             location: CLLocationCoordinate2D(
-                latitude: 32.7911123, longitude: -117.2540975)),
+                latitude: 32.7911123,
+                longitude: -117.2540975
+            )
+        ),
         Bar(
             name: "The Beverly Beach Garden",
             location: CLLocationCoordinate2D(
-                latitude: 32.7924436, longitude: -117.2544375)),
+                latitude: 32.7924436,
+                longitude: -117.2544375
+            )
+        ),
         Bar(
             name: "Riptides Pb",
             location: CLLocationCoordinate2D(
-                latitude: 32.7959306, longitude: -117.2510682)),
+                latitude: 32.7959306,
+                longitude: -117.2510682
+            )
+        ),
         Bar(
             name: "PB Avenue",
             location: CLLocationCoordinate2D(
-                latitude: 32.7977653, longitude: -117.2506176)),
+                latitude: 32.7977653,
+                longitude: -117.2506176
+            )
+        ),
         Bar(
             name: "Moonshine Beach",
             location: CLLocationCoordinate2D(
-                latitude: 32.7980179, longitude: -117.2484153)),
+                latitude: 32.7980179,
+                longitude: -117.2484153
+            )
+        ),
         Bar(
             name: "PB Shore Club",
             location: CLLocationCoordinate2D(
-                latitude: 32.7942403, longitude: -117.2558471)),
+                latitude: 32.7942403,
+                longitude: -117.2558471
+            )
+        ),
         Bar(
             name: "Society PB",
             location: CLLocationCoordinate2D(
-                latitude: 32.7975231, longitude: -117.2506688)),
+                latitude: 32.7975231,
+                longitude: -117.2506688
+            )
+        ),
         Bar(
             name: "Lahaina Beach House",
             location: CLLocationCoordinate2D(
-                latitude: 32.7916952, longitude: -117.2551161)),
+                latitude: 32.7916952,
+                longitude: -117.2551161
+            )
+        ),
         Bar(
             name: "Break Point",
             location: CLLocationCoordinate2D(
-                latitude: 32.7970878, longitude: -117.2526739)),
+                latitude: 32.7970878,
+                longitude: -117.2526739
+            )
+        ),
         Bar(
             name: "Dirty Birds",
             location: CLLocationCoordinate2D(
-                latitude: 32.7987627, longitude: -117.256312)),
+                latitude: 32.7987627,
+                longitude: -117.256312
+            )
+        ),
         Bar(
             name: "bar Ella",
             location: CLLocationCoordinate2D(
-                latitude: 32.7976868, longitude: -117.2512401)),
+                latitude: 32.7976868,
+                longitude: -117.2512401
+            )
+        ),
         Bar(
             name: "Alehouse",
             location: CLLocationCoordinate2D(
-                latitude: 32.7943251, longitude: -117.2552584)),
+                latitude: 32.7943251,
+                longitude: -117.2552584
+            )
+        ),
         Bar(
             name: "The Duck Dive",
             location: CLLocationCoordinate2D(
-                latitude: 32.7985307, longitude: -117.2562483)),
+                latitude: 32.7985307,
+                longitude: -117.2562483
+            )
+        ),
         Bar(
             name: "PB Local",
             location: CLLocationCoordinate2D(
-                latitude: 32.7936288, longitude: -117.2541387)),
+                latitude: 32.7936288,
+                longitude: -117.2541387
+            )
+        ),
         Bar(
             name: "Firehouse",
             location: CLLocationCoordinate2D(
-                latitude: 32.7947949, longitude: -117.2555755)),
+                latitude: 32.7947949,
+                longitude: -117.2555755
+            )
+        ),
     ]
 
-
-    private func fetchBarLocation(_ bar: String) async -> CLLocationCoordinate2D? {
+    private func fetchBarLocation(_ bar: String) async
+        -> CLLocationCoordinate2D?
+    {
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = bar
         request.region = .init(
-            center: pacificBeachCoordinate, latitudinalMeters: 1000,
-            longitudinalMeters: 1000)
+            center: pacificBeachCoordinate,
+            latitudinalMeters: 1000,
+            longitudinalMeters: 1000
+        )
 
         let result = try? await MKLocalSearch(request: request).start()
 
@@ -120,8 +188,11 @@ final class MapViewModel: ObservableObject {
         guard let barLocation = await fetchBarLocation(bar) else { return }
         cameraPosition = .region(
             .init(
-                center: barLocation, latitudinalMeters: 300,
-                longitudinalMeters: 300))
+                center: barLocation,
+                latitudinalMeters: 300,
+                longitudinalMeters: 300
+            )
+        )
     }
 
     private func getUserLocation() async -> CLLocationCoordinate2D? {
