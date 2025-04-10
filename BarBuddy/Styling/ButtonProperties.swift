21e71160fa04a24dee863b51373da1f51c55be5e
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-04-10 16:10:50 -0700

    Stores button properties for voting menus

---
+++ BarBuddy/Styling/ButtonProperties.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonBehavior.swift
+//  BarBuddy
+//
+//  Created by Andrew Betancourt on 4/9/25.
+//
+
+import Foundation
+
+struct ButtonProperties {
+    var selectedOption: Bool = false
+    var showMenu: Bool = false
+    var offset: CGFloat = 0
+    var type: String
+}
