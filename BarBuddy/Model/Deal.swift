ef05a0acc82ef028092d0131c21a0df17c033496
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-04-16 14:31:22 -0700

    Reverting changes

---
+++ BarBuddy/Model/Deal.swift
@@ -0,0 +1,34 @@
+//
+//  Deal.swift
+//  BarBuddy
+//
+//  Created by Andrew Betancourt on 3/25/25.
+//
+
+import Foundation
+
+struct Deal: Identifiable {
+    
+    let id = UUID()
+    let title: String
+    let location: String
+    let timeDescription: String
+    let description: String
+    let day: [String]
+    
+//    var daysString: String {
+//        if description.count == 7 {
+//            return "Daily"
+//        } else if description.count == 1 {
+//            return description[0].rawValue + "s"
+//        } else {
+//            return description.map { $0.rawValue }.joined(separator: ", ")
+//        }
+//    }
+    
+    func matchesSearch(query: String) -> Bool {
+        return title.lowercased().contains(query.lowercased()) ||
+               location.lowercased().contains(query.lowercased()) ||
+               description.lowercased().contains(query.lowercased())
+    }
+}
---
+++ BarBuddy/Model/Event.swift
@@ -0,0 +1,35 @@
+//
+//  Event.swift
+//  BarBuddy
+//
+//  Created by Andrew Betancourt on 3/25/25.
+//
+
+import Foundation
+
+// DATA MODELS
+struct Event: Identifiable, Searchable {
+    
+    let id = UUID()
+    let title: String
+    let location: String
+    let timeDescription: String
+    let description: String
+    let day: String
+    
+//    var daysString: String {
+//        if description.count == 7 {
+//            return "Daily"
+//        } else if description.count == 1 {
+//            return description[0].rawValue + "s"
+//        } else {
+//            return description.map { $0.rawValue }.joined(separator: ", ")
+//        }
+//    }
+    
+    func matchesSearch(query: String) -> Bool {
+        return title.lowercased().contains(query.lowercased()) ||
+               location.lowercased().contains(query.lowercased()) ||
+               timeDescription.lowercased().contains(query.lowercased())
+    }
+}
