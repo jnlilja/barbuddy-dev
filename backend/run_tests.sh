f348b54f72d991080c71f4f73a6cc8c09be6b29e
Jacob Tang <jake.tang@gmail.com>
2025-04-18 02:43:07 -0700

    Delete backend/run_tests.sh

--- backend/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Set GDAL environment variables
-export GDAL_LIBRARY_PATH="/opt/homebrew/lib/libgdal.dylib"
-export GEOS_LIBRARY_PATH="/opt/homebrew/lib/libgeos_c.dylib"
-export DYLD_LIBRARY_PATH="/opt/homebrew/lib:$DYLD_LIBRARY_PATH"
-
-# Run the tests
-python manage.py test apps.bars.tests.test_models 
