87856330035eec4b183d089cc51c4f536318ec1e
JacobTang06 <jake.tang@gmail.com>
2025-04-21 12:13:47 -0700

    Add requirement

--- backend/barbuddy_api/settings.py
+++ backend/barbuddy_api/settings.py
@@ -180,3 +180,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# media settings
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
