a3ea68446f86ae5ef5d39e5ed68c459b765229a7
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-04-27 14:39:58 -0700

    Started adding tests

--- BarBuddyTests/BarBuddyTests.swift
+++ BarBuddyTests/BarBuddyTests.swift
@@ -8,10 +8,50 @@
 import Testing
 @testable import BarBuddy
 
-struct BarBuddyTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite("Password Validation")
+struct PasswordValidationTest {
+    
+    @MainActor
+    @Test func testSuccessfulPasswordValidation() async throws {
+        let user = SignUpViewModel()
+        user.email = "test@test.com"
+        user.newUsername = "testUser"
+        user.newPassword = "Testing123!"
+        user.validateAndSignUp()
+        #expect(user.alertMessage != "Password must be ≥ 8 characters with a number & special char.")
     }
+    
+    @MainActor
+    @Test func testFailedPasswordValidation() async throws {
+        let user = SignUpViewModel()
+        user.email = "test@test.com"
+        user.newUsername = "testUser"
+        user.newPassword = "Testing"
+        user.validateAndSignUp()
+        #expect(user.alertMessage == "Password must be ≥ 8 characters with a number & special char.")
+    }
+}
 
+@Suite("Email Validation")
+struct EmailValidaionTest {
+
+    @MainActor
+    @Test func testSuccessfulEmailValidation() async throws {
+        let user = SignUpViewModel()
+        user.email = "test@test.com"
+        user.newUsername = "testUser"
+        user.newPassword = "Testing123!"
+        user.validateAndSignUp()
+        #expect(user.alertMessage != "Please enter a valid email.")
+    }
+    
+    @MainActor
+    @Test func testFailedEmailValidation() async throws {
+        let user = SignUpViewModel()
+        user.email = "test"
+        user.newUsername = "testUser"
+        user.newPassword = "Testing123!"
+        user.validateAndSignUp()
+        #expect(user.alertMessage == "Please enter a valid email.")
+    }
 }
