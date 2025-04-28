abd8b14bcd363f996a4a7522875558c43a8d8216
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-04-28 00:16:30 -0700

    Test Config

--- BarBuddyUITests/BarBuddyUITestsLaunchTests.swift
+++ BarBuddyUITests/BarBuddyUITestsLaunchTests.swift
@@ -20,7 +20,9 @@ final class BarBuddyUITestsLaunchTests: XCTestCase {
     @MainActor
     func testLaunch() throws {
         let app = XCUIApplication()
+        app.launchArguments.append("--UITests")
         app.launch()
+        XCTAssertTrue(app.wait(for: .runningForeground, timeout: 30)) // Increase timeout as needed
 
         // Insert steps here to perform after app launch but before taking a screenshot,
         // such as logging into a test account or navigating somewhere in the app
@@ -28,6 +30,7 @@ final class BarBuddyUITestsLaunchTests: XCTestCase {
         let attachment = XCTAttachment(screenshot: app.screenshot())
         attachment.name = "Launch Screen"
         attachment.lifetime = .keepAlways
+        
         add(attachment)
     }
 }
