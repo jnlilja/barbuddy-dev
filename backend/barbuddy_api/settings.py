54fbddecb43845378c35273a334e4946c72117a5
BrandonGarate177 <brandongarate177@gmail.com>
2025-06-05 14:00:57 -0700

    Update settings.py

--- backend/barbuddy_api/settings.py
+++ backend/barbuddy_api/settings.py
@@ -97,6 +97,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "barbuddy_api.middleware.RequestLoggingMiddleware",
     "barbuddy_api.middleware.RequestDebugMiddleware",
 ]
 
