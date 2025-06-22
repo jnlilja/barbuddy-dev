4789fd58feb578be416b87d2cefb6f441f34f9da
Andrew Betancourt <andrewbetancourt00@gmail.com>
2025-06-22 13:35:36 -0700

    Better network error handling for vote submission

--- BarBuddy/Model/BarHours.swift
+++ BarBuddy/Model/BarHours.swift
@@ -15,4 +15,14 @@ struct BarHours: Codable, Identifiable, Hashable {
     let openTime: Date?
     let closeTime: Date?
     var isClosed: Bool
+    
+    var displayHours: String {
+        guard let openTime = openTime, let closeTime = closeTime else {
+            return "Hours unavailable"
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        return "\(dateFormatter.string(from: openTime)) - \(dateFormatter.string(from: closeTime))"
+    }
 }
